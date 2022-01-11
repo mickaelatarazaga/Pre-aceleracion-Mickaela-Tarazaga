99d60a68611148454db7afce24d1974fcaf2aa85
mickaelatarazaga <tarazaga.mickaela@gmail.com>
2022-01-11 17:57:01 -0300

    initial commit

---
+++ src/main/java/com/alkemy/DisneyAPIMickaelaTarazaga/DisneyApiMickaelaTarazagaApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.DisneyAPIMickaelaTarazaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApiMickaelaTarazagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApiMickaelaTarazagaApplication.class, args);
+	}
+
+}
